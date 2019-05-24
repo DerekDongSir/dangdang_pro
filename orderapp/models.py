fa17a4e7f37f24af613a7e72d12b7b41bf36b452
DerekDong <derekdongsir@163.com>
2019-05-24 23:05:12 +0800

    定义了city model,并向数据库中导入省，市，区县三级联动数据

--- orderapp/models.py
+++ orderapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    id = models.IntegerField(primary_key=True)
+    pid = models.IntegerField(null=True)
+    cityname = models.CharField(max_length=255,null=True)
+    type = models.IntegerField(null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'city'
