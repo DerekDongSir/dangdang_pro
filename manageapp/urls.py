c04943ecedafcbdf67627c0cb50dd7cb4853f9f0
DerekDong <derekdongsir@163.com>
2019-05-26 13:38:57 +0800

    研究了checkbox实现单选，全选，取消全选的功能； 在书籍list表中实现了单选删除，全选删除的功能；

--- manageapp/urls.py
+++ manageapp/urls.py
@@ -4,10 +4,13 @@ app_name ='manageapp'
 urlpatterns = [
     path('index/',views.index,name='index'),
     path('booklist/',views.booklist,name='booklist'),
+    path('delete_book/',views.delete_book,name="delete_book"),
     path('add_book/',views.add_book,name='add_book'),
+    path('add_book_logic/',views.add_book_logic,name='add_book_logic'),
     path('add_book_type/',views.add_book_type,name='add_book_type'),
     path('add_book_type_logic/',views.add_book_type_logic,name='add_book_type_logic'),
     path('add_parent_type/',views.add_parent_type,name='add_parent_type'),
+    path('add_parent_type_logic/',views.add_parent_type_logic,name='add_parent_type_logic'),
     path('book_id/',views.book_id,name='book_id'),
     path('location/',views.location,name='location'),
 ]
--- manageapp/views.py
+++ manageapp/views.py
@@ -20,14 +20,35 @@ def booklist(request):
     book_dic = {book.catogray.catogry_name:book for book in page}
     return render(request, 'manageapp/bookIist.html',{'page':page,'book_dic':book_dic})
 
+def delete_book(request):
+    book_id = request.GET.get('book_id')
+    book = BookInfo.objects.get(id=book_id)
+    # book.delete()
+    return HttpResponse('ok')
+
 
 def add_parent_type(request):
     return render(request,'manageapp/add_parent_type.html')
 
 
+def add_parent_type_logic(request):
+    '''
+    :param request:接收到的父类名称
+    :return:
+    '''
+    type_name = request.GET.get('type_name') # 将父类名称添加入类别库中
+    # print(type_name)
+
+    return HttpResponse('ok')
+
+
 def add_book_type(request):
-    parents = TCatogray.objects.filter(parent_id=None)
+    parents = request.session.get('parents')
+    if not parents:
+        parents = TCatogray.objects.filter(parent_id=None)
+        request.session['parents'] = parents
     return render(request,'manageapp/add_book_type.html',{"parents":parents})
+
 
 def add_book_type_logic(request):
     type_name = request.GET.get('t_name')
@@ -45,10 +66,32 @@ def book_id(request):
 
 
 def add_book(request):
-    return render(request,'manageapp/add_book.html')
+    catogray = request.session.get('catogray')
+    if not catogray:
+        catogray = TCatogray.objects.filter(parent_id__isnull=False)#筛选出所有的二级类
+        request.session['catogray'] = catogray
+    return render(request,'manageapp/add_book.html',{'catogray':catogray})
+
+
+def add_book_logic(request):
+    book_name = request.GET.get('book_name')
+    author = request.GET.get('author')
+    press = request.GET.get('press')
+    catogray_id = request.GET.get('catogray_id')
+    publish_time = request.GET.get('publish_time')
+    # print(catogray_id)
+    return HttpResponse('ok')
 
 
 def location(request):
-    return render(request,'manageapp/location.html')
+    page_num = int(request.GET.get('page_num',1))
+    locations = request.session.get('locations')
+    if not locations:
+        locations = TDelivery.objects.filter(torder__create_time__isnull=False) #筛选出创建过订单的用户地址信息
+        request.session['locations'] = locations
+    paginator = Paginator(locations,per_page=10)
+    page = paginator.page(page_num)
+    location_dict ={ location.torder_set.all()[0].create_time:location for location in page }
+    return render(request,'manageapp/location.html',{'page':page,'location_dict':location_dict})
 
 
